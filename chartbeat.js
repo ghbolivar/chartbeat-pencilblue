a0ae92b0bca77728f3244f2f93ba4047bbfbd584
Brian Hyder <brianhyder@gmail.com>
2015-03-27 08:45:48 -0400

    Converted for 0.4.0

--- chartbeat.js
+++ chartbeat.js
@@ -1,67 +1,71 @@
-/**
- * Chartbeat - A chartbeat analytics plugin
- *
- * @author Brian Hyder <brian@pencilblue.org>
- * @copyright 2014 PencilBlue, LLC
- */
-function Chartbeat(){}
 
-/**
- * Called when the application is being installed for the first time.
- *
- * @param cb A callback that must be called upon completion.  cb(err, result).
- * The result is ignored
- */
-Chartbeat.onInstall = function(cb) {
-    cb(null, true);
-};
+module.exports = function(pb) {
+    
+    /**
+     * Chartbeat - A chartbeat analytics plugin
+     *
+     * @author Brian Hyder <brian@pencilblue.org>
+     * @copyright 2014 PencilBlue, LLC
+     */
+    function Chartbeat(){}
 
-/**
- * Called when the application is uninstalling this plugin.  The plugin should
- * make every effort to clean up any plugin-specific DB items or any in function
- * overrides it makes.
- *
- * @param cb A callback that must be called upon completion.  cb(err, result).
- * The result is ignored
- */
-Chartbeat.onUninstall = function(cb) {
-    cb(null, true);
-};
+    /**
+     * Called when the application is being installed for the first time.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    Chartbeat.onInstall = function(cb) {
+        cb(null, true);
+    };
 
-/**
- * Called when the application is starting up. The function is also called at
- * the end of a successful install. It is guaranteed that all core PB services
- * will be available including access to the core DB.
- *
- * @param cb A callback that must be called upon completion.  cb(err, result).
- * The result is ignored
- */
-Chartbeat.onStartup = function(cb) {
-    pb.AnalyticsManager.registerProvider('chartbeat', function(req, session, ls, cb) {
-        pb.plugins.getSetting('chartbeat_uid', 'chartbeat-pencilblue', function(err, chartbeatUID) {
-            if(!chartbeatUID || chartbeatUID.length === 0) {
-                cb(null, '');
-                return;
-            }
-            var website = pb.config.siteRoot.split('http://').join('').split('https://').join('');
-            var script  = "<script type=\"text/javascript\">var _sf_startpt=(new Date()).getTime()</script><script type=\"text/javascript\">var _sf_async_config = { uid: " + chartbeatUID + ", domain: '" + website + "', useCanonical: true }; (function() { function loadChartbeat() { window._sf_endpt = (new Date()).getTime(); var e = document.createElement('script'); e.setAttribute('language', 'javascript'); e.setAttribute('type', 'text/javascript'); e.setAttribute('src','//static.chartbeat.com/js/chartbeat.js'); document.body.appendChild(e);}; var oldonload = window.onload; window.onload = (typeof window.onload != 'function') ? loadChartbeat : function() { oldonload(); loadChartbeat(); };})();</script>";
+    /**
+     * Called when the application is uninstalling this plugin.  The plugin should
+     * make every effort to clean up any plugin-specific DB items or any in function
+     * overrides it makes.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    Chartbeat.onUninstall = function(cb) {
+        cb(null, true);
+    };
 
-            cb(null, script);
+    /**
+     * Called when the application is starting up. The function is also called at
+     * the end of a successful install. It is guaranteed that all core PB services
+     * will be available including access to the core DB.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    Chartbeat.onStartup = function(cb) {
+        pb.AnalyticsManager.registerProvider('chartbeat', function(req, session, ls, cb) {
+            pb.plugins.getSetting('chartbeat_uid', 'chartbeat-pencilblue', function(err, chartbeatUID) {
+                //TODO handle error
+                if(!chartbeatUID || chartbeatUID.length === 0) {
+                    return cb(err, '');
+                }
+                var website = pb.config.siteRoot.split('http://').join('').split('https://').join('');
+                var script  = "<script type=\"text/javascript\">var _sf_startpt=(new Date()).getTime()</script><script type=\"text/javascript\">var _sf_async_config = { uid: " + chartbeatUID + ", domain: '" + website + "', useCanonical: true }; (function() { function loadChartbeat() { window._sf_endpt = (new Date()).getTime(); var e = document.createElement('script'); e.setAttribute('language', 'javascript'); e.setAttribute('type', 'text/javascript'); e.setAttribute('src','//static.chartbeat.com/js/chartbeat.js'); document.body.appendChild(e);}; var oldonload = window.onload; window.onload = (typeof window.onload != 'function') ? loadChartbeat : function() { oldonload(); loadChartbeat(); };})();</script>";
+
+                cb(null, script);
+            });
         });
-    });
-    cb(null, true);
-};
+        cb(null, true);
+    };
 
-/**
- * Called when the application is gracefully shutting down.  No guarantees are
- * provided for how much time will be provided the plugin to shut down.
- *
- * @param cb A callback that must be called upon completion.  cb(err, result).
- * The result is ignored
- */
-Chartbeat.onShutdown = function(cb) {
-    cb(null, true);
-};
+    /**
+     * Called when the application is gracefully shutting down.  No guarantees are
+     * provided for how much time will be provided the plugin to shut down.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    Chartbeat.onShutdown = function(cb) {
+        cb(null, true);
+    };
 
-//exports
-module.exports = Chartbeat;
+    //exports
+    return Chartbeat;
+};
